0d3abc92e0570ca5c0ddc005e9ddd207d7ac928a
mrzaxaryan <my@domain.com>
2026-01-30 00:09:09 +0400

    fix: update launch configurations to use lldb-dap and adjust program paths

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ fi
 sudo apt-get update
 
 # --- install toolchain
-sudo apt-get install -y "clang-${LLVM_VER}" "clang++-${LLVM_VER}" "lld-${LLVM_VER}" "llvm-${LLVM_VER}"
+sudo apt-get install -y "clang-${LLVM_VER}" "clang++-${LLVM_VER}" "lld-${LLVM_VER}" "llvm-${LLVM_VER}" "lldb-${LLVM_VER}"
 
 # --- point defaults to this version (safe to repeat)
 sudo update-alternatives --install /usr/bin/clang clang "/usr/bin/clang-${LLVM_VER}" 100
@@ -39,6 +39,7 @@ sudo update-alternatives --install /usr/bin/lld lld "/usr/bin/lld-${LLVM_VER}" 1
 sudo update-alternatives --install /usr/bin/llvm-objdump llvm-objdump "/usr/bin/llvm-objdump-${LLVM_VER}" 100
 sudo update-alternatives --install /usr/bin/llvm-objcopy llvm-objcopy "/usr/bin/llvm-objcopy-${LLVM_VER}" 100
 sudo update-alternatives --install /usr/bin/llvm-strings llvm-strings "/usr/bin/llvm-strings-${LLVM_VER}" 100
+sudo update-alternatives --install /usr/bin/lldb-dap lldb-dap "/usr/bin/lldb-dap-${LLVM_VER}" 100
 
 sudo update-alternatives --set clang "/usr/bin/clang-${LLVM_VER}"
 sudo update-alternatives --set clang++ "/usr/bin/clang++-${LLVM_VER}"
@@ -46,3 +47,4 @@ sudo update-alternatives --set lld "/usr/bin/lld-${LLVM_VER}"
 sudo update-alternatives --set llvm-objdump "/usr/bin/llvm-objdump-${LLVM_VER}"
 sudo update-alternatives --set llvm-objcopy "/usr/bin/llvm-objcopy-${LLVM_VER}"
 sudo update-alternatives --set llvm-strings "/usr/bin/llvm-strings-${LLVM_VER}"
+sudo update-alternatives --set lldb-dap "/usr/bin/lldb-dap-${LLVM_VER}"
