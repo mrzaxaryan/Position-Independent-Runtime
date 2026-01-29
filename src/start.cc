e98cebbf4b41f532978d4ccca9fbe55adeb2c093
mrzaxaryan <mrzaxaryan>
2026-01-29 08:14:28 -0800

    feat: restore and enhance test suite execution logging in start.cc

--- src/start.cc
+++ src/start.cc
@@ -10,87 +10,87 @@ ENTRYPOINT INT32 _start(VOID)
 	BOOL allPassed = TRUE;
 
 	Logger::Info<WCHAR>(L"=== CPP-PIC Test Suite ==="_embed);
-	// Logger::Info<WCHAR>(L""_embed);
+	Logger::Info<WCHAR>(L""_embed);
 
-	// // Run all test suites (Embedded/Primitives -> BAL -> PAL -> RAL)
+	// Run all test suites (Embedded/Primitives -> BAL -> PAL -> RAL)
 
-	// // BAL - Embedded Types and Numeric Primitives (bal/types/)
-	// if (!Uint64Tests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	// BAL - Embedded Types and Numeric Primitives (bal/types/)
+	if (!Uint64Tests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// if (!Int64Tests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	if (!Int64Tests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// if (!DoubleTests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	if (!DoubleTests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// if (!StringTests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	if (!StringTests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// // BAL - Data Structures, String Utilities, and Algorithms (bal/core, bal/string, bal/algorithms)
-	// if (!ArrayStorageTests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	// BAL - Data Structures, String Utilities, and Algorithms (bal/core, bal/string, bal/algorithms)
+	if (!ArrayStorageTests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// if (!StringFormatterTests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	if (!StringFormatterTests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// if (!Djb2Tests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	if (!Djb2Tests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// if (!Base64Tests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	if (!Base64Tests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// // PAL (Platform Abstraction Layer) - Memory and System (pal/)
-	// if (!MemoryTests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	// PAL (Platform Abstraction Layer) - Memory and System (pal/)
+	if (!MemoryTests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// if (!RandomTests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	if (!RandomTests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// // RAL (Runtime Abstraction Layer) - Cryptography and Network
-	// if (!ShaTests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	// RAL (Runtime Abstraction Layer) - Cryptography and Network
+	if (!ShaTests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// if (!EccTests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	if (!EccTests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// if (!SocketTests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	if (!SocketTests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// if (!TlsTests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	if (!TlsTests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// if (!DnsTests::RunAll())
-	// 	allPassed = FALSE;
-	// Logger::Info<WCHAR>(L""_embed);
+	if (!DnsTests::RunAll())
+		allPassed = FALSE;
+	Logger::Info<WCHAR>(L""_embed);
 
-	// if (!WebSocketTests::RunAll())
-	// 	allPassed = FALSE;
+	if (!WebSocketTests::RunAll())
+		allPassed = FALSE;
 
-	// // Final summary
-	// Logger::Info<WCHAR>(L"=== Test Suite Complete ==="_embed);
-	// if (allPassed)
-	// {
-	// 	Logger::Info<WCHAR>(L"ALL TESTS PASSED!"_embed);
-	// }
-	// else
-	// {
-	// 	Logger::Error<WCHAR>(L"SOME TESTS FAILED!"_embed);
-	// }
+	// Final summary
+	Logger::Info<WCHAR>(L"=== Test Suite Complete ==="_embed);
+	if (allPassed)
+	{
+		Logger::Info<WCHAR>(L"ALL TESTS PASSED!"_embed);
+	}
+	else
+	{
+		Logger::Error<WCHAR>(L"SOME TESTS FAILED!"_embed);
+	}
 
 	ExitProcess(allPassed ? 0 : 1);
 }
