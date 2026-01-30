54b8426b26253d2915f0c26cb8820b29849e878f
mr <mr>
2026-01-30 10:51:17 -0800

    Refactor Console::Write methods to use NtWriteFile for improved output handling

--- src/pal/windows/console.cc
+++ src/pal/windows/console.cc
@@ -1,23 +1,23 @@
 #include "console.h"
 #include "pal.h"
-#include "kernel32.h"
+#include "ntdll.h"
 #include "peb.h"
 
 UINT32 Console::Write(const CHAR *text, USIZE length)
 {
 	PPEB peb = GetCurrentPEB();
-	UINT32 numberOfCharsWritten = 0;
-	// Call the WriteConsoleA function
-	Kernel32::WriteConsoleA(peb->ProcessParameters->StandardOutput, text, length, &numberOfCharsWritten, NULL);
-	return numberOfCharsWritten;
+	IO_STATUS_BLOCK ioStatusBlock{};
+	// Use NtWriteFile - works with both console and redirected output (pipes)
+	NTDLL::NtWriteFile(peb->ProcessParameters->StandardOutput, NULL, NULL, NULL, &ioStatusBlock, (PVOID)text, length, NULL, NULL);
+	return (UINT32)ioStatusBlock.Information;
 }
 
 UINT32 Console::Write(const WCHAR *text, USIZE length)
 {
 	PPEB peb = GetCurrentPEB();
-	UINT32 numberOfCharsWritten = 0;
-	// Call the WriteConsoleW function
-	Kernel32::WriteConsoleW(peb->ProcessParameters->StandardOutput, text, length, &numberOfCharsWritten, NULL);
-
-	return numberOfCharsWritten;
+	IO_STATUS_BLOCK ioStatusBlock{};
+	// Use NtWriteFile - works with both console and redirected output (pipes)
+	// Note: Writing wide chars directly; length is in characters, multiply by sizeof(WCHAR) for bytes
+	NTDLL::NtWriteFile(peb->ProcessParameters->StandardOutput, NULL, NULL, NULL, &ioStatusBlock, (PVOID)text, length * sizeof(WCHAR), NULL, NULL);
+	return (UINT32)(ioStatusBlock.Information / sizeof(WCHAR));
 }
