5e0e84dbd79e9214e39c7b18bf7b9cc95c023c7b
CelesteNostdlib <rnostdlib@gmail.com>
2026-02-26 03:27:43 -0800

    ref: implement BinaryWriter class

---
+++ include/core/binaryWriter.h
@@ -0,0 +1,32 @@
+#include "primitives.h"
+#include "memory.h"
+
+
+class BinaryWriter{
+    private:
+        PVOID address;
+        USIZE offset;
+        USIZE maxSize;
+    public:
+        BinaryWriter(PVOID address, USIZE offset, USIZE maxSize) : address(address), offset(offset), maxSize(maxSize) {}
+        template<typename T>
+        static PVOID Write(BinaryWriter* writer, T value){
+            if(writer->offset + sizeof(T) > writer->maxSize){
+                return nullptr;
+            }
+
+            *(T*)((PCHAR)writer->address + writer->offset) = value;
+            writer->offset += sizeof(T);
+            return writer->address;
+        }
+
+        static PVOID WriteBytes(BinaryWriter* writer, PCHAR data, USIZE size){
+            if(writer->offset + size > writer->maxSize){
+                return nullptr;
+            }
+
+            Memory::Copy((PCHAR)writer->address + writer->offset, data, size);
+            writer->offset += size;
+            return writer->address;
+        }
+};
